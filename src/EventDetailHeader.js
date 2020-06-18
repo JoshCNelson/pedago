64a3469eeb617cf640b4abc200b96791dc1d6487
Josh Nelson <joshcdnelson@gmail.com>
2020-06-18 08:21:37 -0400

    save point

--- src/EventDetailHeader.js
+++ src/EventDetailHeader.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './EventDetailHeader.css';
-
 const EventDetailHeader = () => {
   return (
     <div
--- src/EventDetailModal.js
+++ src/EventDetailModal.js
@@ -16,25 +16,32 @@ const EventDetailModal = (props) => {
   }
 
   return (
-    <div
-      style={{ position: "relative" }}
-      className="modal">
+    <div className="modal">
       <div
-        onClick={onClick}
-        style={{
-          color: "white",
-          position: "absolute",
-          right: "10px",
-          top: "10px",
-          padding: "10px",
-          cursor: "pointer"
-        }}
-      > X</div>
+        className="close"
+        onClick={onClick}>
+        X
+      </div>
       <EventDetailHeader />
-      <div>Date & Time</div>
-      <div>Description</div>
-      <div>Location</div>
-      <div>Map</div>
+      <div className="details">
+        <div>
+          <h2>Date & Time</h2>
+          <p>Wednesday, August 28</p>
+          <p>5:00 PM - 7:00 PM PST</p>
+        </div>
+        <div>
+          <h2>Description</h2>
+          <p>A long desc goes here. It should cut..</p>
+          <p>Read More</p>
+        </div>
+        <div>
+          <h2>Location</h2>
+          <p>The Tech Interactive</p>
+          <p>201 S Market St</p>
+          <p>San Jose, CA 95113</p>
+          <p>Get Directions</p>
+        </div>
+      </div>
       <EventDetailFooter />
     </div >
   );
